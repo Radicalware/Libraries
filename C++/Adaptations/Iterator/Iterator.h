028fcb6969a2b06f88386e7dff037b2f8b6a8671
Scourge <Scourge@protonmail.com>
2018-08-20 00:57:27 -0400

    Iterator

--- C++/Adaptations/Iterator/Iterator.h
+++ C++/Adaptations/Iterator/Iterator.h
@@ -21,10 +21,10 @@
 * limitations under the License.
 */
 
-    // The iterator is an adaptation from the following
+    // The iterator is an adaptation from the following content
 
     // cs.helsinki.fi/u/tpkarkka/alglib/k06/lectures/iterators.html
-    // and perfectly insane's content
+    // dreamincode.net/forums/topic/58468-making-your-own-iterators
 
 
 using size_t = unsigned long int;
@@ -46,6 +46,8 @@ public:
             iterator(T* ptr) : m_ptr(ptr) { }
             iterator operator+=(size_t num) { m_ptr += num; return *this; }
             iterator operator-=(size_t num) { m_ptr -= num; return *this; }
+            T*       operator+ (size_t num) { return (m_ptr + num); }
+            T*       operator- (size_t num) { return (m_ptr - num); }
             iterator operator++() { m_ptr++; return *this; }
             iterator operator--() { m_ptr--; return *this; }
             bool     operator==(const iterator& other) { return m_ptr == other.m_ptr; }
@@ -61,12 +63,14 @@ public:
             const_iterator(T* ptr) : m_ptr(ptr) { }
             const_iterator operator+=(size_t num) { m_ptr += num; return *this; }
             const_iterator operator-=(size_t num) { m_ptr -= num; return *this; }
-            const_iterator operator++() { m_ptr++; return *this; }
-            const_iterator operator--() { m_ptr--; return *this; }
+            const T*       operator+ (size_t num) { return (m_ptr + num); }
+            const T*       operator- (size_t num) { return (m_ptr - num); }
+            const_iterator operator++(){ m_ptr++; return *this; }
+            const_iterator operator--(){ m_ptr--; return *this; }
             bool           operator==(const const_iterator& other) { return m_ptr == other.m_ptr; }
             bool           operator!=(const const_iterator& other) { return m_ptr != other.m_ptr; }
-            const T&       operator*()  { return *m_ptr; }
-            const T*       operator->() { return  m_ptr; }
+            const T&       operator*() { return *m_ptr; }
+            const T*       operator->(){ return  m_ptr; }
     };
 
     class reverse_iterator{
@@ -76,12 +80,14 @@ public:
             reverse_iterator(T* ptr) : m_ptr(ptr) { }
             reverse_iterator operator+=(size_t num) { m_ptr -= num; return *this; }
             reverse_iterator operator-=(size_t num) { m_ptr += num; return *this; }
-            reverse_iterator operator++() { *this; m_ptr--; return *this; }
-            reverse_iterator operator--() { *this; m_ptr++; return *this; }
-            bool           operator==(const reverse_iterator& other) { return m_ptr == other.m_ptr; }
-            bool           operator!=(const reverse_iterator& other) { return m_ptr != other.m_ptr; }
-            const T&       operator*()  { return *m_ptr; }
-            const T*       operator->() { return  m_ptr; }
+            T*               operator+ (size_t num) { return (m_ptr - num); }
+            T*               operator- (size_t num) { return (m_ptr + num); }
+            reverse_iterator operator++(){ *this; m_ptr--; return *this; }
+            reverse_iterator operator--(){ *this; m_ptr++; return *this; }
+            bool             operator==(const reverse_iterator& other) { return m_ptr == other.m_ptr; }
+            bool             operator!=(const reverse_iterator& other) { return m_ptr != other.m_ptr; }
+            T&               operator*() { return *m_ptr; }
+            T*               operator->(){ return  m_ptr; }
     };
 
     class const_reverse_iterator{
@@ -91,55 +97,34 @@ public:
             const_reverse_iterator(T* ptr) : m_ptr(ptr) { }
             const_reverse_iterator operator+=(size_t num) { m_ptr -= num; return *this; }
             const_reverse_iterator operator-=(size_t num) { m_ptr += num; return *this; }
+            const T*               operator+ (size_t num) { return (m_ptr - num); }
+            const T*               operator- (size_t num) { return (m_ptr + num); }
             const_reverse_iterator operator++() { m_ptr--; return *this; }
             const_reverse_iterator operator--() { m_ptr++; return *this; }
             bool           operator==(const const_reverse_iterator& other) { return m_ptr == other.m_ptr; }
             bool           operator!=(const const_reverse_iterator& other) { return m_ptr != other.m_ptr; }
-            const T&       operator*()  { return *m_ptr; }
-            const T*       operator->() { return  m_ptr; }
+            const T&       operator*() { return *m_ptr; }
+            const T*       operator->(){ return  m_ptr; }
     };
 
-    // -----------------------------------------------------------------
-    iterator begin(){
-        return iterator(*m_array);
-    }
-    iterator end(){
-        return iterator(*m_array + *m_size);
-    }
-    const_iterator cbegin() const{
-        return const_iterator(*m_array);
-    }
-    const_iterator cend() const{
-        return const_iterator(*m_array + *m_size);
-    }
-    const_iterator begin() const{
-        return const_iterator(*m_array);
-    }
-    const_iterator end() const{
-        return const_iterator(*m_array + *m_size);
-    }
-    // -----------------------------------------------------------------
-    reverse_iterator rbegin(){
-        return reverse_iterator(*m_array + *m_size-1);
-    }
-    reverse_iterator rend(){
-        return reverse_iterator(*m_array-1);
-    }
-    const_reverse_iterator crbegin() const{
-        return const_reverse_iterator(*m_array + *m_size-1);
-    }
-    const_reverse_iterator crend() const{
-        return const_reverse_iterator(*m_array-1);
-    }
-    const_reverse_iterator rbegin() const{
-        return const_reverse_iterator(*m_array + *m_size-1);
-    }
-    const_reverse_iterator rend() const{
-        return const_reverse_iterator(*m_array-1);
-    }
-    // -----------------------------------------------------------------
-    void set_array(T** array){m_array = array;}
-    void set_size(size_t* size){m_size = size;}
+    // --------------------------------------------------------------------------------------------
+    iterator begin(){ return iterator(*m_array); }
+    iterator end()  { return iterator(*m_array + *m_size); }
+    const_iterator cbegin() const{ return const_iterator(*m_array); }
+    const_iterator cend()   const{ return const_iterator(*m_array + *m_size); }
+    const_iterator begin()  const{ return const_iterator(*m_array); }
+    const_iterator end()    const{ return const_iterator(*m_array + *m_size); }
+    // --------------------------------------------------------------------------------------------
+    reverse_iterator rbegin(){ return reverse_iterator(*m_array + *m_size-1); }
+    reverse_iterator rend()  { return reverse_iterator(*m_array-1); }
+    const_reverse_iterator crbegin() const{ return const_reverse_iterator(*m_array + *m_size-1); }
+    const_reverse_iterator crend()   const{ return const_reverse_iterator(*m_array-1);}
+    const_reverse_iterator rbegin()  const{ return const_reverse_iterator(*m_array + *m_size-1); }
+    const_reverse_iterator rend()    const{ return const_reverse_iterator(*m_array-1); }
+    // --------------------------------------------------------------------------------------------
+    void set_array(T** array)  { m_array = array; }
+    void set_size(size_t* size){ m_size  = size;  }
+    // --------------------------------------------------------------------------------------------
 
 // ============================================================================================================
 };
