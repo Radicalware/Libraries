10f8737747ed1400ece5bb036f114d65fb288a72
Scourge <scourge@protonmail.com>
2018-09-24 10:45:56 -0400

    Update Timer.cpp

--- C++/Adaptations/Timer/Timer.cpp
+++ C++/Adaptations/Timer/Timer.cpp
@@ -1,18 +1,16 @@
 #include<iostream>
 #include<chrono>
-using namespace std;
+#include "Timer.h"
 
-#include "./Timer.h"
 // Lib by learncpp.com
 
-Timer::Timer() : m_beg(clock_t::now())
-{   }
+Timer::Timer() : m_beg(clock_t::now()){   }
 
-void Timer::reset(){ 
-    m_beg = clock_t::now(); 
+
+void Timer::reset() {
+	m_beg = clock_t::now();
 }
 
-double Timer::elapsed() const  { 
-    return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();    
+double Timer::elapsed() const {
+	return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
 }
-
