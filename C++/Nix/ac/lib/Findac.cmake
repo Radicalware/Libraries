1567167345634ef8d552404c64bd3e175e1fc39d
Scourge <Scourge@protonmail.com>
2019-03-30 15:59:37 -0500

    Array Type Controller 1.2.2

--- C++/Nix/ac/lib/Findac.cmake
+++ C++/Nix/ac/lib/Findac.cmake
@@ -1,4 +1,3 @@
-
 # Finder for ac.h
 
 cmake_minimum_required(VERSION 3.10)
@@ -6,33 +5,18 @@ cmake_minimum_required(VERSION 3.10)
 # Set the project name
 project (ac_lib)
 
-set(AC_DIR /opt/Radicalware/Libraries/cpp/code/ac)
-find_path(
-    AC_INCLUDE_DIR ac_lib.h
-    PATH_SUFFIXES include PATHS
-    ${AC_DIR}
-)
-
-
-set(POTENTIAL_AC_LIBS ac aclib libac)
-
-find_library(ac_lib
-  NAMES ${POTENTIAL_AC_LIBS}
-    PATH_SUFFIXES lib
-  PATHS
-    ${AC_DIR}
-)
-
 add_definitions(
     -Wfatal-errors
     -std=c++17
     -O2
 )
 
-add_library(${PROJECT_NAME} SHARED ${AC_DIR}/src/ac.cpp)
-add_library(gen::ac ALIAS ${PROJECT_NAME})
+set(ac_DIR /opt/Radicalware/Libraries/cpp/code/ac)
+
+add_library(${PROJECT_NAME} SHARED ${ac_DIR}/src/ac.cpp)
+add_library(rad::ac ALIAS ${PROJECT_NAME})
 
 target_include_directories(${PROJECT_NAME}
     PUBLIC
-        ${AC_DIR}/include
+        ${ac_DIR}/include
 )
