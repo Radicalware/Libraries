fd04a6cbdd8a3f58a2a34c92ba920ac3327cfd98
Scourge <Scourge@protonmail.com>
2019-04-02 00:08:46 -0500

    --(ADP v1.0.3) Console Colors

--- C++/Nix/Adaptations/cc/lib/Findcc.cmake
+++ C++/Nix/Adaptations/cc/lib/Findcc.cmake
@@ -1,4 +1,3 @@
-
 # Finder for cc.h
 
 cmake_minimum_required(VERSION 3.10)
@@ -6,35 +5,18 @@ cmake_minimum_required(VERSION 3.10)
 # Set the project name
 project (cc_lib)
 
-
-set(cc_dir /opt/Radicalware/Libraries/cpp/code/cc)
-find_path(
-    cc_include_dir cc_lib.h
-    PATH_SUFFIXES include PATHS
-    ${cc_dir}
-)
-
-set(potential_cc_libs cc cc_lib cclib libcc)
-
-find_library(cc_lib
-  NAMES ${potential_cc_libs}
-    PATH_SUFFIXES lib
-  PATHS
-    ${cc_dir}
-)
-
 add_definitions(
     -Wfatal-errors
     -std=c++17
     -O2
 )
 
-add_library(${PROJECT_NAME} SHARED ${cc_dir}/src/cc.cpp)
-add_library(gen::cc ALIAS ${PROJECT_NAME})
+set(cc_DIR /opt/Radicalware/Libraries/cpp/code/cc)
+
+add_library(${PROJECT_NAME} SHARED ${cc_DIR}/src/cc.cpp)
+add_library(rad::cc ALIAS ${PROJECT_NAME})
 
 target_include_directories(${PROJECT_NAME}
     PUBLIC
-        ${cc_dir}/include
+        ${cc_DIR}/include
 )
-
-
