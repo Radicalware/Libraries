bab8d6158c72b27faa0853558f4db643eca3cc13
Scourge <Scourge@protonmail.com>
2019-12-21 23:52:03 -0700

    Libs v13.0.0

--- C++/Xplatform/General_Purpose_Libs/Socket/Examples/Solution/src/ex_Socket.cpp
+++ C++/Xplatform/General_Purpose_Libs/Socket/Examples/Solution/src/ex_Socket.cpp
@@ -7,10 +7,10 @@
 using std::cout;
 using std::endl;
 
-// In this example, the client will send the server a name "King Conan"
-// Then the server will use a function (lambda) to create a welcom message for the name
-// then the server will send that welcom message back to the client
-// lastly we will have the client print out the servers welcome message
+// In this example, the client will send the server a name "Joel Leagues"
+// Then the server will use a function (lambda) to create a welcome message for the name
+// then the server will send that welcome message back to the client
+// lastly we will have the client print out the servers welcome message after running a copy
 
 int main(int argc, char** argv)
 {
@@ -26,7 +26,7 @@ int main(int argc, char** argv)
             socket.server.set_function([](const xstring* client_data) -> xstring { return xstring("Welcome: '") + *client_data + "'\n"; });
 
             socket.server.listen("5555").accept().recv(50).respond().close();
-            // accept(4) allows the first 4 chars so "King" out of "King Conan"
+            // accept(4) allows the first 4 chars so "Joel" out of "Joel Leagues"
             // accept()  allows an infinite amount
         }
         catch (const std::runtime_error & err) {
@@ -38,7 +38,7 @@ int main(int argc, char** argv)
     {
         Socket socket(Socket::Start::Client);
         try {
-            socket.client.connect("127.0.0.1", "5555").send("King Conan").recv(50).close();
+            socket.client.connect("127.0.0.1", "5555").send("Joel Leagues").recv(50).close();
             // recv() >> will continue to recieve all the bytes
             // recv(6) >> will gather 6 bytes so we will get "welcom" from the message
         }
