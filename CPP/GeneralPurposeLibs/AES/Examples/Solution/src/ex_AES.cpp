91046e2579df62f294a1837975584d9c3e47925b
Scourge <Scourge@Protonmail.com>
2021-11-23 00:58:51 -0800

    Libs v21.0.0

--- CPP/GeneralPurposeLibs/AES/Examples/Solution/src/ex_AES.cpp
+++ CPP/GeneralPurposeLibs/AES/Examples/Solution/src/ex_AES.cpp
@@ -14,11 +14,8 @@ int main(int arc, char *argv[])
     Begin();
     Nexus<>::Start();
 
-    xstring Plaintext;
-    Plaintext  = "This is my secret message, don't abuse it!";
-
+    xstring Plaintext = "This is my secret message, don't abuse it!";
     cout << Plaintext.size() << endl;
-
     {
         RA::AES Encryption;
         Encryption.SetPlainText(Plaintext);
@@ -30,7 +27,6 @@ int main(int arc, char *argv[])
         Encryption.GetCipherText().ToByteString().Print("\n\n");
         Encryption.Decrypt().Print();
     }
-
     {
         RA::AES Encryption;
         xstring ByteStr = "\\x32\\x56\\x5D\\xBC\\x95\\xDC\\x44\\xEC\\x03\\x08\\x35\\x40\\xF1\\x33\\x36\\xC0\\x9E\\xE5\\x9E\\x47\\x76\\x4C\\xF3\\xAC\\x96\\xAE\\x3C\\xDF\\xCD\\x2F\\x91\\x74\\x4B\\x6F\\x73\\xD0\\xE1\\xB5\\xE1\\xB5\\x55\\xA0";
@@ -40,7 +36,6 @@ int main(int arc, char *argv[])
         Encryption.SetIV("123456781234335678");
         Encryption.SetTag("\xE5\xCD\x80\xF4\xA3");
         Encryption.Decrypt().Print();
-
     }
 
     RescuePrint();
